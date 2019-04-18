f1c226583c8fc13712e58a27e0c9e3619a59cbc3
agleevtimur <42980610+agleevtimur@users.noreply.github.com>
2019-04-18 10:35:14 +0300

    формула

--- TextProject/FunctionClass.cs
+++ TextProject/FunctionClass.cs
@@ -6,8 +6,37 @@ using System.Threading.Tasks;
 
 namespace TextProject
 {
-    class FunctionClass
+    public class FunctionClass<T>
     {
+        public static List<T> GetKeys(Dictionary<T, int> dict)
+        {
+            dict.OrderByDescending(x => x.Value);
+            var count = 0;// сколько ключевых слов
+            if (dict.Count < 6 && dict.Count > 2)
+            {// на маленьких словарях резкий упадок частоты не может гарантировать корректность результата
+                count = 3;// пример ильдара показал, что ближе всего число 3
+            }
+            else
+            {
+                var max = int.MinValue;
+                int prev = 0;
+                var index = 0;
+                foreach (var value in dict.Values)
+                {// резкий упадок
+                    if (prev - value > max)
+                    {
+                        max = prev - value;
+                        count = index;
+                    }
+                    index++;
+                    prev = value;
+                }
+            }
 
+            return dict
+                    .Select(x => x.Key)
+                    .Take(count)
+                    .ToList();
+        }
     }
 }
