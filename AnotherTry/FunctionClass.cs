0e41362bb8858f4373c6a74b6b22d53c2e2840d3
Nesom <rakhimovsamatarrr@gmail.com>
2019-05-10 12:55:29 +0300

    Попробуйте щас это

---
+++ AnotherTry/FunctionClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcMovie
+{
+    public static class FunctionClass<T>
+    {
+        public static List<T> GetKeys(Dictionary<T, int> dict)
+        {
+            dict = dict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x=> x.Value);
+            var count = 0;// сколько ключевых слов
+            if (dict.First().Value < 10 || dict.Count < 6 && dict.Count > 2)
+            {// на маленьких словарях резкий упадок частоты не может гарантировать корректность результата
+                count = 3;// пример ильдара показал, что ближе всего число 3
+            }
+            else
+            {
+                var max = int.MinValue;
+                int prev = 0;
+                var index = 0;
+                foreach (var value in dict.Values)
+                {// резкий упадок
+                    if (prev - value > max)
+                    {
+                        max = prev - value;
+                        count = index;
+                    }
+                    index++;
+                    prev = value;
+                }
+            }
+
+            return dict
+                    .Select(x => x.Key)
+                    .Take(count)
+                    .ToList();
+        }
+    }
+}
---
+++ AnotherTry/Views/HelloWorld/Index.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcMovie.Views.HelloWorld
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel()
+        {
+
+        }
+        public string Answer { get; set; }
+    }
+}
