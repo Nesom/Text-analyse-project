1d723698f9a2d499178a7908cb6f4c110e410ca2
Nesom <rakhimovsamatarrr@gmail.com>
2019-05-10 12:19:27 +0300

    It's my not final final version

---
+++ AbrProject/Controllers/HelloWorldController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        //FunctionClass<string> r = new FunctionClass<string>();
+        //AnalyseHelper tr = new AnalyseHelper();
+        Class c1 = new Class();
+            //[HttpPost]
+        public IActionResult Index()
+        {
+            return View("~/Views/HelloWorld/Index.cshtml", new MvcMovie.Views.HelloWorld.IndexModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(string text)
+        {
+            var form = HttpContext.Request.Form;
+            var answer = form["answerid"];
+            var b = string.Empty;
+            var a = TextAnalyzer.Analyze(answer.ToString());
+            foreach(var e in a)
+            {
+                
+                b += " " + e;
+            }
+            c1.Answer = text + "a";
+            return View(new Views.HelloWorld.IndexModel() { Answer = b});
+        }
+        // 
+        // GET: /HelloWorld/Welcome/
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
